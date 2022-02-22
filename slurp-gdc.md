40a23f8ef4d299e9ac5cafb488d9649d1e3f53a7
Mark A. Jensen <mark.jensen@nih.gov>
2022-02-22 13:01:09 -0500

    add some pod
    plus markdown version

---
+++ slurp-gdc.md
@@ -0,0 +1,34 @@
+# NAME
+
+slurp-gdc.pl - Create MDF representing GDC model in gdcdictionary repo
+
+## SYNOPSIS
+
+    $ export SCHEMADIR=gdcdictionary/gdcdictionary/schemas
+    # create gdc-model.yaml.new, gdc-model-props.yaml.new, gdc-model-terms.yaml.new
+    $ perl slurp-gdc.pl
+
+## DESCRIPTION
+
+`slurp-gdc.pl` reads YAML "Gen3" schema configuration files as provided in
+the open source [repo](https://github.com/NCI-GDC/gdcdictionary), and 
+converts it into [Model Description File](https://github.com/CBIIT/bento-mdf)
+format. Specifically, by default, it reads from a submodule version of the
+GDC repo and outputs three files
+
+- gdc-model.yaml.new
+- gdc-model-props.yaml.new
+- gdc-model-terms.yaml.new
+
+These may need futzing with, depending on the downstream YAML parser, because
+of inconsistent escaping of single quotes (apostrophes, e.g.) in the GDC source.
+
+Because the Term definitions are so crazy, this script url-escapes the term 
+definitions before writing them to the term.yaml file (see ["model-desc/gdc-model-terms.yaml" in .](./model-desc/gdc-model-terms.yaml)).
+Need to unescape these before using the text downstream.
+
+# DEPENDENCIES
+
+Use [cpanminus](https://cpanmin.us) as follows to install dependencies:
+
+    $ cpanm JSON::ize URI::Escape YAML::XS YAML::PP JSON::XS Try::Tiny
--- slurp-gdc.pl
+++ slurp-gdc.pl
@@ -74,14 +74,59 @@ for my $t (sort {$a->value cmp $b->value} $dict->terms) {
   }
 }
 
-# open my $gdc, ">gdc-model.yaml" or die $!;
-# print $gdc Dump($mdf);
-# close $gdc;
-# open my $gdcp, ">gdc-model-props.yaml" or die $!;
-# print $gdcp Dump($propdefs);
-# close $gdcp;
+open my $gdc, ">gdc-model.yaml.new" or die $!;
+print $gdc Dump($mdf);
+close $gdc;
+open my $gdcp, ">gdc-model-props.yaml.new" or die $!;
+print $gdcp Dump($propdefs);
+close $gdcp;
 
-open my $gdct, ">gdc-model-terms.2.yaml" or die $!;
+open my $gdct, ">gdc-model-terms.yaml.new" or die $!;
 print $gdct Dump($terms);
 close $gdct;
 1;
+
+
+=head1 NAME
+
+slurp-gdc.pl - Create MDF representing GDC model in gdcdictionary repo
+
+=head2 SYNOPSIS
+
+ $ export SCHEMADIR=gdcdictionary/gdcdictionary/schemas
+ # create gdc-model.yaml.new, gdc-model-props.yaml.new, gdc-model-terms.yaml.new
+ $ perl slurp-gdc.pl
+
+=head2 DESCRIPTION
+
+C<slurp-gdc.pl> reads YAML "Gen3" schema configuration files as provided in
+the open source L<repo|https://github.com/NCI-GDC/gdcdictionary>, and 
+converts it into L<Model Description File|https://github.com/CBIIT/bento-mdf>
+format. Specifically, by default, it reads from a submodule version of the
+GDC repo and outputs three files
+
+=over
+
+=item * gdc-model.yaml.new
+
+=item * gdc-model-props.yaml.new
+
+=item * gdc-model-terms.yaml.new
+
+=back
+
+These may need futzing with, depending on the downstream YAML parser, because
+of inconsistent escaping of single quotes (apostrophes, e.g.) in the GDC source.
+
+Because the Term definitions have many issues of this type, this
+script just url-escapes the term definitions before writing them to
+the term.yaml file (see L<./model-desc/gdc-model-terms.yaml>). Need
+to unescape these before using the text downstream.
+
+=head1 DEPENDENCIES
+
+Use L<cpanminus|https://cpanmin.us> as follows to install dependencies:
+
+ $ cpanm JSON::ize URI::Escape YAML::XS YAML::PP JSON::XS Try::Tiny
+
+=cut
